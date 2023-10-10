6a22548465ac2693a94d1b9a557f0a8104694db5
GeorgyFirsov <gfirsov007@gmail.com>
2023-10-10 21:34:11 +0000

    chore: tea1 key register initialization
    Signed-off-by: GeorgyFirsov <gfirsov007@gmail.com>

--- include/details/tea1.h
+++ include/details/tea1.h
@@ -7,6 +7,24 @@
 #ifndef TETRALIB_TEA1_INCLUDED
 #define TETRALIB_TEA1_INCLUDED
 
+/**
+ * @brief Key type for TEA1. The algorithm has 80 bit key.
+ */
+typedef unsigned char tea1_key_t[10];
 
+
+/**
+ * @brief Key state for TEA1.
+ */
+typedef unsigned char tea1_key_state_t[4];
+
+
+/**
+ * @brief Key register initialization function.
+ * 
+ * @param key user-supplied key
+ * @param key_register output register (MUST contain all zeros)
+ */
+void tea1_initialize_key_register(const tea1_key_t* key, tea1_key_state_t* key_register);
 
 #endif  // !TETRALIB_TEA1_INCLUDED
